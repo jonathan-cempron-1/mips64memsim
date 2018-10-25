3912f7e1abf2013261f9b94b3a9d0b38d1c0ab0f
jonats <jp.cempron@gmail.com>
2018-10-25 12:28:59 +0800

    initial commit initial gui

---
+++ mips64bm/src/mips64bm/rtd/RuntimeData.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mips64bm.rtd;
+import mips64bm.gui.*;
+
+/**
+ *
+ * @author jonats
+ */
+public class RuntimeData {
+    public RuntimeData(){
+        new FrmWindow1(this);
+    }
+}
